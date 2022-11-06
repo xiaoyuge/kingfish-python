645f48b8b8525df43b6fc6262f991853dc5ad07b
xiaoyuge <xiaoyuge@gmail.com>
2022-11-07 04:47:41 +0800

    测试多线程爬虫的时间

--- concurrent/multi_thread_craw.py
+++ concurrent/multi_thread_craw.py
@@ -5,12 +5,16 @@
 
 import blog_spider
 import threading
+import time 
 
 def single_thread_craw():
+    print("single_thread_craw start")
     for url in blog_spider.urls:
         blog_spider.craw(url)
+    print("single_thread_craw end")
         
 def multi_thread_craw():
+    print("multi_thread_craw start")
     threads = []
     for url in blog_spider.urls:
         threads.append(
@@ -21,3 +25,15 @@ def multi_thread_craw():
         
     for thread in threads:
         thread.join()
+    print("multi_thread_craw end")
+    
+if __name__ == "__main__":
+    start = time.time()
+    single_thread_craw()
+    end = time.time();
+    print("single_thread_craw cost: ",end-start,"seconds")
+    
+    start = time.time()
+    multi_thread_craw()
+    end = time.time();
+    print("multi_thread_craw cost: ",end-start,"seconds")
