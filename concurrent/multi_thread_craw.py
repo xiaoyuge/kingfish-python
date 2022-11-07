b8e4e46eb0e469912074de9162ec8e7e420315a6
xiaoyuge <xiaoyuge@gmail.com>
2022-11-07 12:52:11 +0800

    完善readme

--- concurrent/multi_thread_craw.py
+++ concurrent/multi_thread_craw.py
@@ -32,11 +32,13 @@ def multi_thread_craw():
     print("multi_thread_craw end")
     
 if __name__ == "__main__":
+    #单线程爬取，统计一下时间
     start = time.time()
     single_thread_craw()
     end = time.time();
     print("single_thread_craw cost: ",end - start,"seconds")
     
+    #多线程爬取，统计一下时间
     start = time.time()
     multi_thread_craw()
     end = time.time();
