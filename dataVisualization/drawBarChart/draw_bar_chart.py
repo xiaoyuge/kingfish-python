fbb8774e66c3341e3f554444e50567ab5ddb7627
xiaoyuge <xiaoyuge@gmail.com>
2022-11-07 20:04:05 +0800

    draw bar chart

---
+++ dataVisualization/drawBarChart/draw_bar_chart.py
@@ -0,0 +1,26 @@
+"""
+@author kingfish
+读取excel数据，画柱状图
+"""
+
+import pandas as pd
+
+from pyecharts import options as opts
+from pyecharts.charts import Bar
+
+fpath = "dataVisualization/drawBarChart/商家销售数据.xlsx"
+
+excel = pd.read_excel(fpath)
+
+print(excel.head)
+print(excel.columns)
+
+bar = (
+    Bar()
+    .add_xaxis(excel["商品"].tolist())
+    .add_yaxis("商家A",excel["商家A"].tolist())
+    .add_yaxis("商家B",excel["商家B"].tolist())
+    .set_global_opts(title_opts=opts.TitleOpts(title="商品销量对比图"))
+)
+
+bar.render_notebook()
