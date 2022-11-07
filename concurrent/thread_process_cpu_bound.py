77511878240a3b79a189360904b9cab67cb95ee9
xiaoyuge <xiaoyuge@gmail.com>
2022-11-07 17:43:40 +0800

    对于CPU密集型业务，对比单线程、多线程和多进程的性能

---
+++ concurrent/thread_process_cpu_bound.py
@@ -0,0 +1,65 @@
+"""
+@author kingifsh
+对于CPU密集型业务，对比单线程、多线程和多进程的性能
+在我的双核CPU的MAC本上，跑的结果如下：
+single_thread cost : 62.778594970703125 seconds
+multi_thread cost : 63.94747805595398 seconds
+multi_process cost : 35.869956254959106 seconds
+"""
+
+import math
+import concurrent.futures
+import time
+
+primes = [112272535095293] * 100
+
+#计算是否是素数
+def is_prime(n):
+    if n<2 :
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    sqrt_n = int(math.floor(math.sqrt(n)))
+    for i in range(3,sqrt_n+1,2):
+        if n % i==0:
+            return False
+    return True
+
+#用单线程计算
+def single_thread():
+    for number in primes:
+        is_prime(number)
+        
+#用多线程计算
+def multi_thread():
+    with concurrent.futures.ThreadPoolExecutor() as pool:
+        pool.map(is_prime,primes)
+        
+#用多进程计算
+def multi_process():
+    with concurrent.futures.ProcessPoolExecutor() as pool:
+        pool.map(is_prime,primes)
+
+#实际运行一下，看看各自花费的时间
+if __name__ == "__main__":
+    start = time.time();
+    single_thread()
+    end = time.time();
+    print("single_thread cost :",end - start,"seconds")
+    
+    start = time.time();
+    multi_thread()
+    end = time.time();
+    print("multi_thread cost :",end - start,"seconds")
+    
+    start = time.time();
+    multi_process()
+    end = time.time();
+    print("multi_process cost :",end - start,"seconds")
+
+        
+
+
+
