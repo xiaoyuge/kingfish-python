8c45ca03846e61d7629952ddb70f69fe4a2dfb85
xiaoyuge <xiaoyuge@gmail.com>
2022-11-07 11:41:29 +0800

    完善html爬取和解析函数

--- concurrent/blog_spider.py
+++ concurrent/blog_spider.py
@@ -3,6 +3,7 @@
 爬取博客园网站的网页数据
 """
 import requests
+from bs4 import BeautifulSoup
 
 urls = [
     f"https://www.cnblogs.com/#p{page}"
@@ -14,7 +15,15 @@ print(urls)
 #爬取函数
 def craw(url):
     r = requests.get(url)
-    print(url,len(r.text))
+    return r.text
+   
+#解析爬取到的html的标签
+def parse(html):
+    # 假设解析标签是<a>且class="post-item-title"
+    soup = BeautifulSoup(html,"html.parser")
+    links = soup.find_all("a",class_="post-item-title")
+    return [(link["href"],link.get_text()) for link in links]
 
-craw(urls[0])
-
+if __name__ == "__main__":
+    for result in parse(craw(urls[2])):
+        print(result)
