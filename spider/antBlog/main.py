2d011a86b53817772cb0c55a87f829ea1f6b6b6d
xiaoyuge <xiaoyuge@gmail.com>
2022-11-08 12:43:35 +0800

    增加注释

--- spider/antBlog/main.py
+++ spider/antBlog/main.py
@@ -21,8 +21,10 @@ h2s = soup.find_all(name="h2",attrs={"class":"entry-title"})
 #定义一个集合存放所有解析到的链接和标题
 links = set()
 
+#遍历h2节点，找到子节点a，把链接和标题作为元组放入集合
 for h2 in h2s:
     link = h2.find("a")
     links.add((link["href"],link.get_text()))
-    
+
+#看一下我们爬取到的内容
 print(links)
