442da0226a3e664e7f0d5f74be6ffd9ff6eeba66
xiaoyuge <xiaoyuge@gmail.com>
2022-11-09 20:01:15 +0800

    *args 和 **kwargs

--- basic/decorator_pass_param.py
+++ basic/decorator_pass_param.py
@@ -1,6 +1,8 @@
 """
 @author kingfish
 可以在对应的装饰器函数 wrapper() 上，加上相应的参数，来实现原函数 greet() 的参数需传递给装饰器
+如果有多个参数需要传递，则可使用*args和**kwargs
+args 是 arguments 的缩写，表示位置参数；kwargs 是 keyword arguments 的缩写，表示关键字参数
 """
 
 def my_decorator(func):
@@ -14,3 +16,16 @@ def greet(message):
     print(message)
 
 greet('hello world')
+
+def my_decorator2(func):
+    def wrapper(*args, **kwargs):
+        print('wrapper of decorator')
+        func(*args, **kwargs)
+    return wrapper
+
+@my_decorator2
+def greet2(*args):
+    for arg in args:
+        print(arg)
+        
+greet2(123,"ahah",[1,2,3])
