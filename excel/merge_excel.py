a725b719d891ee14ac7a529ddb44fd68b8d92e64
xiaoyuge <xiaoyuge@gmail.com>
2022-11-14 12:20:36 +0800

    修改merge_excel.py的注释

--- excel/merge_excel.py
+++ excel/merge_excel.py
@@ -1,11 +1,18 @@
+"""
+@author kingfish
+两个结构相同的excel文件，拼接到一起，输出到一个文件
+"""
+
 import pandas as pd
 import os
 
 dfs = []
 
 # read all excel 's sheet append to dfs
-for fname in os.listdir("./"):
+fpath = "datas/merge_excel_multi_sheets/"
+for fname in os.listdir(fpath):
     if fname.endswith(".xls") and fname != "final.xls":
+        fname = fpath + fname
         df = pd.read_excel(
             fname,
             header=None,
