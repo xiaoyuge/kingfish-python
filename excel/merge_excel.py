d04777010645bb2975107a5e572dc1af4573c70c
xiaoyuge <xiaoyuge@gmail.com>
2022-11-14 12:28:49 +0800

    完善z注释

--- excel/merge_excel.py
+++ excel/merge_excel.py
@@ -6,22 +6,29 @@
 import pandas as pd
 import os
 
+#用来存放读取的多个文件的内容
 dfs = []
 
-# read all excel 's sheet append to dfs
+#文件的读取目录路径
 fpath = "datas/merge_excel_multi_sheets/"
+
+# read all excel 's sheet append to dfs
+
 for fname in os.listdir(fpath):
     if fname.endswith(".xls") and fname != "final.xls":
+        #拼接一下完整的文件路径
         fname = fpath + fname
+        
         df = pd.read_excel(
             fname,
             header=None,
             sheet_name=None
         )
+        #将读取的文件内容加入列表
         dfs.extend(df.values())
 
-# concat
+# 将多个文件的内容拼接在一起
 result = pd.concat(dfs)
 
 # output excel
-result.to_excel("./final.xls", index=False)
+result.to_excel("%sfinal.xls"%fpath, index=False)
