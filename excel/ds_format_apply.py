45f39eb3ce446a0e1ff2b7f0a8b029e981bf3f76
xiaoyuge <xiaoyuge@gmail.com>
2022-11-15 21:59:09 +0800

    几个对比dataframe遍历性能的脚本

---
+++ excel/ds_format_apply.py
@@ -0,0 +1,99 @@
+"""
+@author kingfish
+这个代码来源于真实的需求，见/data/joyce/需求文档.md
+"""
+
+import pandas as pd
+import xlwings as xw
+import time
+
+#要处理的文件路径
+fpath = "datas/joyce/DS_format_bak.xlsm"
+
+#把CP和DS两个sheet的数据分别读入pandas的dataframe
+#cp_df = ds_format_workbook.sheets["CP"].range("A1").options(pd.DataFrame,expand='table',index=False,numbers=float).value
+cp_df = pd.read_excel(fpath,sheet_name="CP",header=[0])
+ds_df = pd.read_excel(fpath,sheet_name="DS",header=[0,1])
+
+start = time.time()
+#先清空DS表的Delta和LOI列的值
+
+def handle_none(data):
+    if data is None or data == "" or type(data) == None.__class__:
+        return 0
+    return data
+
+def clear_Delta_Loi(row):
+    if row[('Total','Capabity.1')] == 'Delta':
+        row[('Current week','BOH')] = 0
+    if row[('Total','Capabity.1')] == 'LOI':
+        row[('Current week','BOH')] = 0
+    
+ds_df[('Current week','BOH')] = ds_df.apply(clear_Delta_Loi,axis=1)
+
+end = time.time()
+
+print(f"清空DS表的Delta和LOI列的值 time cost is :{end-start} seconds")
+
+start = time.time()
+
+delta_item_group_site_set = set()
+loi_item_group_site_set = set()
+
+def iner_iter_fuc(ds_row_k):
+                  
+    ds_total_capabity1 = ds_row_k[('Total','Capabity.1')]
+        
+    #计算DS表的Delta值
+    if ds_total_capabity1 == 'Delta':
+    #相同的item_group+siteid是否计算过
+        if (key in delta_item_group_site_set) == False:
+            delta_item_group_site_set.add(key)
+            delta_value = handle_none(ds_row_k[('Current week','BOH')])
+            MRP_LOI_value = handle_none((cp_row['MRP (LOI)']))
+            MRP_OOI_value = handle_none(cp_row['MRP (OOI)'])
+            ds_row_k[('Current week','BOH')] = float(delta_value) + float(MRP_LOI_value) + float(MRP_OOI_value)                        
+            print(f"item_group={ds_item_group}-Delta:{ ds_row_k[('Current week','BOH')]}")
+    #计算DS表的LOI值
+    if ds_total_capabity1 == 'LOI':
+        #相同的item_group+siteid是否计算过
+        if (key in loi_item_group_site_set) == False:
+            loi_item_group_site_set.add(key)
+            LOI_value = handle_none(ds_row_k[('Current week','BOH')])
+            MRP_LOI_value = handle_none(cp_row['MRP (LOI)'])
+            ds_row_k[('Current week','BOH')] = float(LOI_value)+ float(MRP_LOI_value)
+            print(f"item_group={ds_item_group}-LOI:{ ds_row_k[('Current week','BOH')]}")
+
+def iter_in_ds(ds_row):
+    
+      #获取DS表的Item_group值
+        global ds_item_group 
+        ds_item_group = ds_row[('Total','Capabity')]
+        
+        if ds_item_group != "" and cp_item_group == ds_item_group :
+            index_j = ds_row.name
+            iner_iter_df = ds_df.loc[index_j:index_j+5]
+            iner_iter_df.apply(iner_iter_fuc,axis=1)
+
+
+def iter_in_cp(data):
+    global cp_row
+    cp_row = data
+    #获取CP表的Item_group和siteid值
+    global cp_item_group
+    cp_item_group = cp_row['Item Group']
+    siteid = cp_row['SITEID']
+    global key 
+    key = cp_item_group + "-" + siteid
+    ds_df.apply(iter_in_ds,axis=1)
+
+cp_df.apply(iter_in_cp,axis=1)
+
+#释放数据
+delta_item_group_site_set.clear()
+loi_item_group_site_set.clear()
+
+end = time.time()
+print(f"计算DS表的Delta和LOI列的值 time cost is :{end-start} seconds")
+
+            
---
+++ excel/ds_format_iterrow.py
@@ -0,0 +1,79 @@
+"""
+@author kingfish
+这个代码来源于真实的需求，见/data/joyce/需求文档.md
+"""
+
+import pandas as pd
+import xlwings as xw
+import time
+
+#要处理的文件路径
+fpath = "datas/joyce/DS_format_bak.xlsm"
+
+#把CP和DS两个sheet的数据分别读入pandas的dataframe
+#cp_df = ds_format_workbook.sheets["CP"].range("A1").options(pd.DataFrame,expand='table',index=False,numbers=float).value
+cp_df = pd.read_excel(fpath,sheet_name="CP",header=[0])
+ds_df = pd.read_excel(fpath,sheet_name="DS",header=[0,1])
+
+start = time.time()
+#先清空DS表的Delta和LOI列的值
+for index,row in ds_df.iterrows():
+    print(row[('Total','Capabity.1')])
+    if row[('Total','Capabity.1')] == 'Delta':
+        row[('Current week','BOH')] = 0
+    if row[('Total','Capabity.1')] == 'LOI':
+        row[('Current week','BOH')] = 0
+end = time.time()
+
+print(f"清空DS表的Delta和LOI列的值 time cost is :{end-start} seconds")
+
+
+start = time.time()
+delta_item_group_site_set = set()
+loi_item_group_site_set = set()
+
+#根据CP和DS表的Item_group值做lookup，计算DS表的Delta值
+for index_i,cp_row in cp_df.iterrows():
+    
+    #获取CP表的Item_group和siteid值
+    cp_item_group = cp_row['Item Group']
+    siteid = cp_row['SITEID']
+    key = cp_item_group + "-" + siteid  
+        
+    for index_j,ds_row in ds_df.iterrows():
+        
+        #获取DS表的Item_group值
+        ds_item_group = ds_row[('Total','Capabity')]
+        
+        if ds_item_group != "" and cp_item_group == ds_item_group :
+           
+            iner_iter_df = ds_df.loc[index_j:index_j+5]
+           
+            for index_k,ds_row_k in iner_iter_df.iterrows():
+                
+                ds_total_capabity1 = ds_row_k[('Total','Capabity.1')]
+        
+                #计算DS表的Delta值
+                if ds_total_capabity1 == 'Delta':
+                    #相同的item_group+siteid是否计算过
+                    if (key in delta_item_group_site_set) == False:
+                        delta_item_group_site_set.add(key)
+                        ds_row_k[('Current week','BOH')] = pd.to_numeric(ds_row_k[('Current week','BOH')]) + pd.to_numeric(cp_row['MRP (LOI)']) + pd.to_numeric(cp_row['MRP (OOI)'])                        
+                        print(f"item_group={ds_item_group}-Delta:{ ds_row_k[('Current week','BOH')]}")
+                #计算DS表的LOI值
+                if ds_total_capabity1 == 'LOI':
+                    #相同的item_group+siteid是否计算过
+                    if (key in loi_item_group_site_set) == False:
+                        loi_item_group_site_set.add(key)
+                        ds_row_k[('Current week','BOH')] = pd.to_numeric(ds_row_k[('Current week','BOH')],errors='coerce') + pd.to_numeric(cp_row['MRP (LOI)'],errors='coerce') 
+                        print(f"item_group={ds_item_group}-LOI:{ ds_row_k[('Current week','BOH')]}")
+                
+
+
+#释放数据
+delta_item_group_site_set.clear()
+loi_item_group_site_set.clear()
+
+end = time.time()
+print(f"计算DS表的Delta和LOI列的值 time cost is :{end-start} seconds")
+            
