a9ae545999fab3ade5e2ab8193698bca0b36f48f
xiaoyuge <xiaoyuge@gmail.com>
2022-11-18 21:25:55 +0800

    apply正确的多线程版本

--- excel/ds_format_apply_right_multi_thread_.py
+++ excel/ds_format_apply_right_multi_thread_.py
@@ -10,6 +10,7 @@ import xlwings as xw
 import time
 import math
 from threading import Thread
+import threading
 
 
 #要处理的文件路径
@@ -50,19 +51,149 @@ def handle_nan(data):
 delta_item_group_site_set = set()
 loi_item_group_site_set = set()
 
+def clear_Delta(row):
+    if row[('Total','Capabity.1')] == 'Delta':
+        print(f"线程-{threading.current_thread().getName()}清除{row[('Total','Capabity')]}的{row[('Total','Capabity.1')]}的值")
+        return 0
+    
+def clear_Loi(row):
+    if row[('Total','Capabity.1')] == 'LOI':
+        print(f"线程-{threading.current_thread().getName()}清除{row[('Total','Capabity')]}的{row[('Total','Capabity.1')]}的值")
+        return 0
+    
+def clear_Demand(ds_row,ds_datetime):
+    ds_item_group = ds_row[('Total','Capabity')]
+    Capabity_1 = ds_row[('Total','Capabity.1')]
+    if Capabity_1 == 'Demand':
+        print(f'线程{threading.current_thread().getName()}清空{ds_item_group}的{Capabity_1}的日期{ds_datetime}的值')
+        return 0
+    
+def clear_supply(ds_row,ds_datetime):
+    ds_item_group = ds_row[('Total','Capabity')]
+    Capabity_1 = ds_row[('Total','Capabity.1')]
+    if Capabity_1 == 'Supply':
+        print(f'线程{threading.current_thread().getName()}清空{ds_item_group}的{Capabity_1}的日期{ds_datetime}的值')
+        return 0
+
+def Cal_Delta_Iter_In_Ds(ds_row):
+    #获取DS表的Item_group值
+    ds_item_group = ds_row[('Total','Capabity')]
+    #获取DS表的Capabity.1的值，用于判断是delta还是loi
+    ds_total_capabity1 = ds_row[('Total','Capabity.1')]
+    
+    if ds_total_capabity1 == 'Delta':
+        return_delta_val = handle_nan(ds_row[('Current week','BOH')])
+        #从cp_df获取item_group相等的一组数据
+        selected_cp_df = cp_df.loc[cp_df['Item Group']==ds_item_group,:]
+        for cp_df_index,cp_df_row in selected_cp_df.iterrows():
+            key = cp_df_row['Item Group'] + '-' + cp_df_row['SITEID']
+            if (key in delta_item_group_site_set) == False:
+                delta_item_group_site_set.add(key)
+                MRP_LOI_value = handle_nan((cp_df_row['MRP (LOI)']))
+                MRP_OOI_value = handle_nan(cp_df_row['MRP (OOI)'])
+                return_delta_val = return_delta_val + pd.to_numeric(MRP_LOI_value,errors='coerce') + pd.to_numeric(MRP_OOI_value,errors='coerce') 
+                print(f"线程{threading.current_thread().getName()}:item_group={ds_item_group}的Delta={ return_delta_val}")
+        return return_delta_val
+
+def Cal_Loi_Iter_In_Ds(ds_row):
+     #获取DS表的Item_group值
+    ds_item_group = ds_row[('Total','Capabity')]
+    #获取DS表的Capabity.1的值，用于判断是delta还是loi
+    ds_total_capabity1 = ds_row[('Total','Capabity.1')]
+        
+    if ds_total_capabity1 == 'LOI':
+        return_LOI_val = handle_nan(ds_row[('Current week','BOH')])
+        #从cp_df获取item_group相等的一组数据
+        selected_cp_df = cp_df.loc[cp_df['Item Group']==ds_item_group,:]
+        for cp_df_index,cp_df_row in selected_cp_df.iterrows():
+            key = cp_df_row['Item Group'] + '-' + cp_df_row['SITEID']
+            if (key in delta_item_group_site_set) == False:
+                LOI_value = handle_nan(ds_row[('Current week','BOH')])
+                MRP_LOI_value = handle_nan(cp_df_row['MRP (LOI)'])
+                return_LOI_val = return_LOI_val + pd.to_numeric(LOI_value,errors='coerce')+ pd.to_numeric(MRP_LOI_value,errors='coerce')
+                print(f"线程{threading.current_thread().getName()}:item_group={ds_item_group}的LOI={ return_LOI_val}")
+        return return_LOI_val
+
+def cal_demand_by_datetime(ds_row,ds_datetime):
+    ds_item_group = ds_row[('Total','Capabity')]
+    Capabity_1 = ds_row[('Total','Capabity.1')]
+    if Capabity_1 == 'Demand':
+        selected_cp_df = cp_df.loc[(cp_df['Item Group']==ds_item_group) & (cp_df['Measure']=='Total Publish Demand'),:]
+        return_damand_val = 0
+        for index_cp_df,cp_df_row in selected_cp_df.iterrows():
+            return_damand_val = return_damand_val + cp_df_row[ds_datetime]
+        print(f"线程{threading.current_thread().getName()}:item_group={ds_item_group}的{Capabity_1}的日期为{ds_datetime}的值={return_damand_val}")
+        return return_damand_val
+
+def cal_supply_by_datetime(ds_row,ds_datetime):
+    ds_item_group = ds_row[('Total','Capabity')]
+    Capabity_1 = ds_row[('Total','Capabity.1')]
+    if Capabity_1 == 'Supply':
+        selected_cp_df = cp_df.loc[(cp_df['Item Group']==ds_item_group) & ((cp_df['Measure']=='Total Commit') | (cp_df['Measure']=='Total Risk Commit')),:]
+        return_supply_val = 0
+        for index_cp_df,cp_df_row in selected_cp_df.iterrows():
+            return_supply_val = return_supply_val + cp_df_row[ds_datetime]
+        print(f"线程{threading.current_thread().getName()}:item_group={ds_item_group}的{Capabity_1}的日期为{ds_datetime}的值={return_supply_val}")
+        return return_supply_val
 
 def p_clear_and_cal_delta():
-    
-    pass
+    #先清除Delta
+    ds_df[('Current week','BOH')] = ds_df.apply(clear_Delta,axis=1)
+    #计算Delta
+    ds_df[('Current week','BOH')] = ds_df.apply(Cal_Delta_Iter_In_Ds,axis=1)
+    #释放计算Delta过程中的临时数据
+    delta_item_group_site_set.clear()
 
 def p_clear_and_cal_loi():
-    pass
+    #先清除LOI
+    ds_df[('Current week','BOH')] = ds_df.apply(clear_Loi,axis=1)
+    #计算LOI
+    ds_df[('Current week','BOH')] = ds_df.apply(Cal_Loi_Iter_In_Ds,axis=1)
+    #释放计算LOI过程中的临时数据
+    loi_item_group_site_set.clear()
 
 def p_clear_and_cal_demand():
-    pass
+    #先清除Demand
+    for i in range(5,len(ds_df.columns)):
+        ds_datetime = ds_df.columns.get_level_values(1)[i]
+        ds_month = ds_df.columns.get_level_values(0)[i]
+        if type(ds_datetime) == str and ds_datetime != "" and (ds_datetime in cp_df.columns):
+            ds_df[(f'{ds_month}',f'{ds_datetime}')] = ds_df.apply(clear_Demand,axis=1,args=(ds_datetime,))
+    #计算Demand
+    for i in range(5,len(ds_df.columns)):
+        ds_datetime = ds_df.columns.get_level_values(1)[i]
+        ds_month = ds_df.columns.get_level_values(0)[i]
+        if type(ds_datetime) == str and ds_datetime != "" and (ds_datetime in cp_df.columns):
+            ds_df[(f'{ds_month}',f'{ds_datetime}')] = ds_df.apply(cal_demand_by_datetime,axis=1,args=(ds_datetime,))
 
 def p_clear_and_cal_supply():
-    pass
+    #先清除Supply
+    for i in range(5,len(ds_df.columns)):
+        ds_datetime = ds_df.columns.get_level_values(1)[i]
+        ds_month = ds_df.columns.get_level_values(0)[i]
+        if type(ds_datetime) == str and ds_datetime != "" and (ds_datetime in cp_df.columns):
+            ds_df[(f'{ds_month}',f'{ds_datetime}')] = ds_df.apply(clear_supply,axis=1,args=(ds_datetime,))
+    #计算Supply
+    for i in range(5,len(ds_df.columns)):
+        ds_datetime = ds_df.columns.get_level_values(1)[i]
+        ds_month = ds_df.columns.get_level_values(0)[i]
+        if type(ds_datetime) == str and ds_datetime != "" and (ds_datetime in cp_df.columns):
+            ds_df[(f'{ds_month}',f'{ds_datetime}')] = ds_df.apply(cal_supply_by_datetime,axis=1,args=(ds_datetime,))
+    
+def handle_item_group_nan_by_row(ds_df_row):
+    #先处理一下DS的item_group值，将Nan填充为真实的item_group值
+    real_ds_item_group = {'key':''}
+    cur_ds_item_group = ds_df_row[('Total','Capabity')]
+    capabity_1 = ds_df_row[('Total','Capabity.1')]
+    if type(cur_ds_item_group) == str and cur_ds_item_group != "" :
+        real_ds_item_group['key'] = cur_ds_item_group
+        return cur_ds_item_group
+    #elif capabity_1 != 'DOI':
+    else:
+        return real_ds_item_group['key']
+
+def handle_item_group_nan():        
+    ds_df[('Total','Capabity')] = ds_df.apply(handle_item_group_nan_by_row,axis=1)
 
 
 if __name__ == "__main__":
@@ -70,6 +201,9 @@ if __name__ == "__main__":
     app_start = time.time()
     #读取excel数据到内存
     read_excel()
+    
+    #先处理一下item_group的nan值
+    handle_item_group_nan()
     
     #开启四个线程开始计算
     cal_start = time.time()
