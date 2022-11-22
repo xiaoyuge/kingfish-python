bcf7933fb3dc9972a89964b8aa38711e22a2ff5d
xiaoyuge <xiaoyuge@gmail.com>
2022-11-22 17:57:57 +0800

    完成初步数据分析

--- crawler/anjuke/app.py
+++ crawler/anjuke/app.py
@@ -3,14 +3,28 @@
 """
 from flask import Flask
 import drawChart as dbc
+import pandas as pd
 
 app = Flask(__name__)
+#读取要分析的数据
+fpath = 'crawler/anjuke/suzhouSecondHouse-2022-11-22-200页.xlsx'
+df = pd.read_excel(fpath,sheet_name="Sheet1",header=[0],engine='openpyxl')
 
 @app.route("/barchart")
 def bar_chart():    
     str = dbc.draw_bar_chart()
     print(str)
     return str   
+
+@app.route("/total_price_analysis_by_suqare")
+def bar_total_price_analysis_by_square():
+    str = dbc.total_price_analysis_by_square(df)
+    return str
+
+@app.route("/unit_price_analysis_by_suqare")
+def bar_unit_price_analysis_by_square():
+    str = dbc.unit_price_analysis_by_square(df)
+    return str
 
 if __name__ == "__main__":
     app.run()
--- crawler/anjuke/drawChart.py
+++ crawler/anjuke/drawChart.py
@@ -3,23 +3,63 @@
 """
 
 import pandas as pd
-
 from pyecharts import options as opts
 from pyecharts.charts import Bar
+    
+def cal_square_district(row):
+    if row['面积'] <= 60:
+        return '[0,60]'
+    if row['面积'] >60 and row['面积'] <= 90:
+        return '[60,90]'
+    if row['面积'] >90 and row['面积'] <= 120:
+        return '[90,120]'
+    if row['面积'] >120 and row['面积'] <= 150:
+        return '[120,150]'
+    if row['面积'] >150 :
+        return '[150,-]'
+    return '[未知]'    
 
-def draw_bar_chart():
-    fpath = "data_visual/drawBarChart/商家销售数据.xlsx"
-
-    excel = pd.read_excel(fpath)
-
+def total_price_analysis_by_square(df):
+    #增加一列[面积区间]
+    df['面积区间'] = df.apply(cal_square_district,args=(),axis=1)
+    #获取要分析的数据行和列
+    analysis_df = df.loc[:,['面积区间','总价']]
+    analysis_df.loc[:,'面积区间'] = analysis_df.loc[:,'面积区间'].astype('str')
+    #对面积区间列group by，然后按分组计算总价和均价的平均值
+    group = analysis_df.groupby('面积区间')
+    group_df = group.mean()
+    group_df.loc[:,'总价'] = group_df.loc[:,'总价'].astype('int')
+    print(group_df)
+    
     bar = (
         Bar()
-        .add_xaxis(excel["商品"].tolist())
-        .add_yaxis("商家A",excel["商家A"].tolist())
-        .add_yaxis("商家B",excel["商家B"].tolist())
-        .set_global_opts(title_opts=opts.TitleOpts(title="商品销量对比图"))
+        .add_xaxis(group_df.index.tolist())
+        .add_yaxis("总价均价",group_df["总价"].tolist())
+        .set_global_opts(title_opts=opts.TitleOpts(title="按面积区间的房屋总价分析"))
     )
     
-def draw_
-
     return bar.render_embed()
+
+def unit_price_analysis_by_square(df):
+    #增加一列[面积区间]
+    df['面积区间'] = df.apply(cal_square_district,args=(),axis=1)
+    #获取要分析的数据行和列
+    analysis_df = df.loc[:,['面积区间','均价']]
+    analysis_df.loc[:,'面积区间'] = analysis_df.loc[:,'面积区间'].astype('str')
+    #对面积区间列group by，然后按分组计算总价和均价的平均值
+    group = analysis_df.groupby('面积区间')
+    group_df = group.mean()
+    group_df.loc[:,'均价'] = group_df.loc[:,'均价'].astype('int')
+    print(group_df)
+    
+    bar = (
+        Bar()
+        .add_xaxis(group_df.index.tolist())
+        .add_yaxis("单价均价",group_df["均价"].tolist())
+        .set_global_opts(title_opts=opts.TitleOpts(title="按面积区间的房屋单价分析"))
+    )
+    
+    return bar.render_embed()
+    
+if __name__ == '__main__':
+    total_price_analysis_by_square()
