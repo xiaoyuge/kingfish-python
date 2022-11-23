752e2ae11cec2d36888fde1aa5e4a7d27c67e5d9
xiaoyuge <xiaoyuge@gmail.com>
2022-11-23 16:28:42 +0800

    优化目录结构

--- crawler/anjuke/app.py
+++ crawler/anjuke/app.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 app = Flask(__name__)
 #读取要分析的数据
-fpath = 'crawler/anjuke/suzhouSecondHouse-2022-11-22-200页.xlsx'
+fpath = 'crawler/anjuke/data/suzhouSecondHouse-2022-11-22-200页.xlsx'
 df = pd.read_excel(fpath,sheet_name="Sheet1",header=[0],engine='openpyxl')
 df.drop_duplicates(keep='first',inplace=True) 
 
@@ -34,6 +34,31 @@ def bar_unit_price_analysis_by_district():
 @app.route("/pie_sale_estate_analysis_by_year")
 def pie_sale_estate_analysis_by_year():
     str = dbc.sale_estate_analysis_by_year(df)
+    return str
+
+@app.route("/histogram_unit_price_analysis")
+def histogram_unit_price_analysis():
+    str = dbc.unit_price_analysis_by_histogram(df)
+    return str
+
+@app.route("/histogram_total_price_analysis")
+def histogram_total_price_analysis():
+    str = dbc.total_price_analysis_by_histogram(df)
+    return str
+
+@app.route("/scatter_unit_price_analysis")
+def scatter_unit_price_analysis():
+    str = dbc.unit_price_analysis_by_scatter(df)
+    return str
+
+@app.route("/word_cloud_hot_word_analysis")
+def word_cloud_hot_word_analysis():
+    str = dbc.hot_word_analysis_by_wordcloud(df)
+    return str
+
+@app.route("/map_unit_price_analysis")
+def map_unit_price_analysis():
+    str = dbc.unit_price_analysis_by_map(df)
     return str
 
 if __name__ == "__main__":
--- crawler/anjuke/drawChart.py
+++ crawler/anjuke/drawChart.py
@@ -4,8 +4,11 @@
 
 import pandas as pd
 from pyecharts import options as opts
-from pyecharts.charts import Bar,Pie
-from pyecharts.commons.utils import JsCode
+from pyecharts.charts import Bar,Pie,Scatter,WordCloud,Map
+import numpy as np
+import jieba
+import jieba.analyse
+
     
 def cal_square_district(row):
     if row['面积'] <= 60:
@@ -30,13 +33,12 @@ def total_price_analysis_by_square(df):
     group = analysis_df.groupby('面积区间')
     group_df = group.mean()
     group_df.loc[:,'总价'] = group_df.loc[:,'总价'].astype('int')
-    print(group_df)
     
     bar = (
         Bar()
         .add_xaxis(group_df.index.tolist())
         .add_yaxis("总价均价",group_df["总价"].tolist())
-        .set_global_opts(title_opts=opts.TitleOpts(title="按面积区间的房屋总价分析"))
+        .set_global_opts(title_opts=opts.TitleOpts(title="苏州二手房按面积区间的房屋总价"))
     )
     
     return bar.render_embed()
@@ -51,13 +53,12 @@ def unit_price_analysis_by_square(df):
     group = analysis_df.groupby('面积区间')
     group_df = group.mean()
     group_df.loc[:,'均价'] = group_df.loc[:,'均价'].astype('int')
-    print(group_df)
     
     bar = (
         Bar()
         .add_xaxis(group_df.index.tolist())
         .add_yaxis("单价均价",group_df["均价"].tolist())
-        .set_global_opts(title_opts=opts.TitleOpts(title="按面积区间的房屋单价分析"))
+        .set_global_opts(title_opts=opts.TitleOpts(title="苏州二手房按面积区间的房屋单价"))
     )
     
     return bar.render_embed()
@@ -76,7 +77,6 @@ def unit_price_analysis_by_estate(df):
     top10_df = group_df.head(10)
     #为了横向柱状图展示，再从低到高排序一下
     top10_df.sort_values('均价',ascending=True,inplace=True)
-    print(top10_df)  
 
     bar = (
         Bar(init_opts=opts.InitOpts(width="1500px"))
@@ -84,7 +84,7 @@ def unit_price_analysis_by_estate(df):
         .add_yaxis("房价单价",top10_df['均价'].tolist())
         .reversal_axis()
         .set_series_opts(label_opts=opts.LabelOpts(position="right"))
-        .set_global_opts(title_opts=opts.TitleOpts(title="苏州小区房价TOP10"),xaxis_opts=opts.AxisOpts(axislabel_opts={'interval':'0'}))
+        .set_global_opts(title_opts=opts.TitleOpts(title="苏州各小区二手房房价TOP10"),xaxis_opts=opts.AxisOpts(axislabel_opts={'interval':'0'}))
     )
     
     return bar.render_embed()
@@ -98,8 +98,7 @@ def unit_price_analysis_by_district(df):
     group_df = group.mean()
     group_df.loc[:,'均价'] = group_df.loc[:,'均价'].astype('int')
     #按照均价列降序排序
-    group_df.sort_values('均价',ascending=True, inplace=True)
-    print(group_df)  
+    group_df.sort_values('均价',ascending=True, inplace=True) 
 
     bar = (
         Bar(init_opts=opts.InitOpts(width="1500px"))
@@ -107,7 +106,7 @@ def unit_price_analysis_by_district(df):
         .add_yaxis("房价单价",group_df['均价'].tolist())
         .reversal_axis()
         .set_series_opts(label_opts=opts.LabelOpts(position="right"))
-        .set_global_opts(title_opts=opts.TitleOpts(title="苏州各区房价排行榜"),xaxis_opts=opts.AxisOpts(axislabel_opts={'interval':'0'}))
+        .set_global_opts(title_opts=opts.TitleOpts(title="苏州各区域二手房房价排行榜"),xaxis_opts=opts.AxisOpts(axislabel_opts={'interval':'0'}))
     )
     
     return bar.render_embed()
@@ -123,7 +122,6 @@ def sale_estate_analysis_by_year(df):
     group = analysis_df.groupby('建筑年份',as_index=False)
     group_df = group.count()
     group_df.loc[:,'待售房屋数'] = group_df.loc[:,'待售房屋数'].astype('int')
-    print(group_df)
     
     pie = Pie(init_opts=opts.InitOpts(width='800px', height='600px', bg_color='white'))
     pie.add("pie",[list(z) for z in zip(group_df['建筑年份'].tolist(),group_df['待售房屋数'].tolist())]
@@ -131,10 +129,122 @@ def sale_estate_analysis_by_year(df):
         ,center=['50%', '50%']
         ,label_opts=opts.LabelOpts(
             position="outside",
-            formatter="{d}%",)
+            formatter="{b}:{c}:{d}%",)
         ).set_global_opts(
-            title_opts=opts.TitleOpts(title='不同建筑年份的待售二手房数', pos_left='300', pos_top='20',
+            title_opts=opts.TitleOpts(title='苏州二手房不同建筑年份的待售数量', pos_left='300', pos_top='20',
             title_textstyle_opts=opts.TextStyleOpts(color='black', font_size=16)),
             legend_opts=opts.LegendOpts(is_show=False))
 
     return pie.render_embed()
+
+def unit_price_analysis_by_histogram(df):
+    hist,bin_edges = np.histogram(df['均价'],bins=100)
+    
+    bar = (
+        Bar()
+        .add_xaxis([str(x) for x in bin_edges[:-1]])
+        .add_yaxis('价格分布',[float(x) for x in hist],category_gap=0)
+        .set_global_opts(
+            title_opts=opts.TitleOpts(title='苏州二手房房价-单价分布-直方图',pos_left='center'),
+            legend_opts=opts.LegendOpts(is_show=False)
+        )
+    )
+    
+    return bar.render_embed()
+
+
+def total_price_analysis_by_histogram(df):
+    hist,bin_edges = np.histogram(df['总价'],bins=100)
+    
+    bar = (
+        Bar()
+        .add_xaxis([str(x) for x in bin_edges[:-1]])
+        .add_yaxis('价格分布',[float(x) for x in hist],category_gap=0)
+        .set_global_opts(
+            title_opts=opts.TitleOpts(title='苏州二手房房价-总价分布-直方图',pos_left='center'),
+            legend_opts=opts.LegendOpts(is_show=False)
+        )
+    )
+    
+    return bar.render_embed()
+
+def unit_price_analysis_by_scatter(df):
+     df.sort_values('面积',ascending=True, inplace=True)
+     
+     square = df['面积'].to_list()
+     unit_price = df['均价'].to_list()
+     
+     scatter = (
+         Scatter()
+         .add_xaxis(xaxis_data=square)
+         .add_yaxis(
+             series_name='',
+             y_axis=unit_price,
+             symbol_size=4,
+             label_opts=opts.LabelOpts(is_show=False)
+         )
+         .set_global_opts(
+             xaxis_opts=opts.AxisOpts(type_='value'),
+             yaxis_opts=opts.AxisOpts(type_='value'),
+             title_opts=opts.TitleOpts(title='苏州二手房面积-单价关系图',pos_left='center')
+         )
+     )
+     
+     return scatter.render_embed()
+ 
+ 
+def hot_word_analysis_by_wordcloud(df):
+    txt = ''
+    for index,row in df.iterrows():
+        txt = txt+ str(row['待售房屋']) + ';'+ str(row['标签']) + '\n'
+     
+    word_weights = jieba.analyse.extract_tags(txt,topK=100,withWeight=True)
+     
+    word_cloud=(
+        WordCloud()
+        .add(series_name='高频词语',data_pair=word_weights,word_size_range=[10,100])
+        .set_global_opts(
+            title_opts=opts.TitleOpts(
+            title='苏州二手房销售热度词',
+            title_textstyle_opts=opts.TextStyleOpts(font_size=23),
+            pos_left='center'
+        )
+    )
+    )
+    
+    return word_cloud.render_embed()
+    
+def transform_name(row):
+    district_name = row['区'].strip()
+    if district_name == '吴中' or district_name == '相城' or district_name == '吴江' or district_name == '虎丘' or district_name == '姑苏':
+        district_name = district_name + '区'
+    if district_name == '常熟' or district_name == '张家港' or district_name == '太仓':
+        district_name = district_name + '市'
+    return district_name
+    
+def unit_price_analysis_by_map(df):
+    data = []
+    #获取要分析的数据列
+    analysis_df = df.loc[:,['区','均价']]
+    #按区列分组
+    group_df = analysis_df.groupby('区',as_index=False)
+    #根据分组对均价列求平均值
+    group_df = group_df.mean('均价')
+    print(group_df)
+    #将区的名字做一下转换，为下面的地图匹配做准备
+    group_df['区'] = group_df.apply(transform_name,axis=1)
+    group_df.loc[:,'均价'] = group_df.loc[:,'均价'].astype('int')
+    #将数据转换成map需要的数据格式
+    for index,row in group_df.iterrows():
+        district_array = [row['区'],row['均价']]
+        data.append(district_array)
+    
+    map = (
+        Map()
+        .add('苏州各区域二手房房价',data,'苏州')
+        .set_global_opts(
+            title_opts=opts.TitleOpts(title='苏州各区域二手房房价地图'),
+            visualmap_opts=opts.VisualMapOpts(max_=26000)
+        )
+    )
+    return map.render_embed()
