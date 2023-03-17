e74f0e2727c3d29adcd49b5e2b587095e76941c5
xiaoyuge <xiaoyuge@gmail.com>
2023-03-17 10:09:42 +0800

    openai api

---
+++ openai/test.py
@@ -0,0 +1,28 @@
+import os
+import openai
+import requests
+import json
+
+openai.organization = "org-EB9Hewj27svx8CRq6TB0RKa3"
+openai.api_key = "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"
+##os.getenv("OPENAI_API_KEY")
+model_list = openai.Model.list()
+## print(model_list)
+
+#尝试请求一下api
+openai_url = "https://api.openai.com/v1/chat/completions"
+headers = {
+    'Content-Type':'application/json',
+    'Authorization':'Bearer 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'
+}
+datas = {
+     "model": "gpt-3.5-turbo",
+     "messages": [{"role": "user", "content": "我想训练一个法律行业的基于chatGPT的微调模型，应该怎么做"}],
+     "temperature": 0.7
+}
+json_data = json.dumps(datas)
+r = requests.post(url=openai_url,data=json_data,headers=headers,timeout=200)
+if r.status_code == 200:
+    resp = r.text
+    print(resp)
+    
