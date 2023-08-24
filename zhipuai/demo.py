4c0be4618b9ae0854d0b1998f333a3a6543b4b2d
xiaoyuge <xiaoyuge@gmail.com>
2023-08-24 20:17:11 +0800

    zhipuai api demo

---
+++ zhipuai/demo.py
@@ -0,0 +1,12 @@
+import zhipuai
+
+zhipuai.api_key = '8895febba7f691d9f067a23913c7fca0.eRVl3bKLcRLSPq6P'
+
+response = zhipuai.model_api.invoke(
+    model = 'chatglm_pro',
+    prompt = [{'role':'user','content':'甲、乙、丙三个人分别住在红、蓝、绿三个颜色的房子里，他们喜欢的水果是苹果、香蕉和橙子，他们每个人都从事不同的职业。1. 在红房子里住的人喜欢苹果。2. 乙是医生。3. 丙住在蓝房子里。4. 不喜欢苹果的人住在绿房子里。问题：甲住在哪个颜色的房子里？'}],
+    top_p=0.7,
+    temperature=0.9
+)
+
+print(response)
